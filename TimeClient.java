5044ad4e627a87a60df07f63c8ad35256d6e7636
Hulcalan <wulch@hawaii.edu>
2015-11-07 21:45:18 -1000

    so it begins
    grabbing the time from an internet source

---
+++ TimeClient.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.net.InetAddress;
+import java.util.Date;
+import org.apache.commons.net.ntp.NTPUDPClient; 
+import org.apache.commons.net.ntp.TimeInfo;
+
+   
+public final class TimeClient
+{
+     
+   public static void main(String[] args) throws Exception {
+      String TIME_SERVER = "time-a.nist.gov";   
+      NTPUDPClient timeClient = new NTPUDPClient();
+      InetAddress inetAddress = InetAddress.getByName(TIME_SERVER);
+      TimeInfo timeInfo = timeClient.getTime(inetAddress);
+      long returnTime = timeInfo.getReturnTime();
+      Date time = new Date(returnTime);
+      System.out.println("Time from " + TIME_SERVER + ": " + time + " " + returnTime);
+   }
+}
