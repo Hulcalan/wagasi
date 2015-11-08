7b5b6666ca2ac7f7dd3bbd62890d66f140247fc7
Hulcalan <wulch@hawaii.edu>
2015-11-08 11:15:01 -1000

    second
    goofing around a bit

--- TimeClient.java
+++ TimeClient.java
@@ -9,12 +9,12 @@ public final class TimeClient
 {
      
    public static void main(String[] args) throws Exception {
-      String TIME_SERVER = "time-a.nist.gov";   
+      String TIME_SERVER = "0.pool.ntp.org";   
       NTPUDPClient timeClient = new NTPUDPClient();
       InetAddress inetAddress = InetAddress.getByName(TIME_SERVER);
       TimeInfo timeInfo = timeClient.getTime(inetAddress);
-      long returnTime = timeInfo.getReturnTime();
+      long returnTime = timeInfo.getMessage().getTransmitTimeStamp().getTime();
       Date time = new Date(returnTime);
-      System.out.println("Time from " + TIME_SERVER + ": " + time + " " + returnTime);
+      System.out.println( "Time Server: " + TIME_SERVER + ": " + time + " " + "\n " + timeInfo.getMessage() );
    }
 }
