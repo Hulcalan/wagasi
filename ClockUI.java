e7fbf71b23d83ca956e64be5e33014773badaa2b
Hulcalan <wulch@hawaii.edu>
2015-11-30 03:39:49 -1000

    Pretty much final
    Just cosmetics need added and a timer mode that's pretty much it!

---
+++ ClockUI.java
@@ -0,0 +1,198 @@
+import java.awt.*;
+import java.io.*;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import javax.swing.event.*;
+import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.imageio.ImageIO;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+
+public class ClockUI extends TimeClient{
+
+
+
+   public ClockUI(Integer value) {
+      EventQueue.invokeLater(
+            new Runnable() {
+               @Override
+               public void run() {
+                    JFrame frame = new JFrame("Test");
+               
+                              frame.setUndecorated(true);
+                  frame.setLayout(new FlowLayout());
+                  
+                 
+               //makes frame translusent
+                  frame.setBackground(new Color(0,0,0,0));
+                
+                  frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                  frame.setLayout(new BorderLayout());
+                  
+                  frame.add(new ClockPane(value));
+                  frame.setAlwaysOnTop( false );
+                  frame.pack();
+                  frame.setLocationRelativeTo(null);
+                  frame.setVisible(true);
+                  frame.toFront();
+                  frame.repaint();
+                 
+               }
+            });
+   }
+
+   public class ClockPane extends JLayeredPane {
+   
+      private BufferedImage circle;
+      private BufferedImage indicator;
+      private double angle = 0;
+      private JButton close;
+      private BufferedImage images;
+      private BufferedImage imagec;
+      private JButton set;
+      private JLabel world = new JLabel();
+      
+      
+      public ClockPane(Integer value) {
+         try {
+            circle = ImageIO.read(new File("./images/picture1.png"));
+            indicator = ImageIO.read(new File("./images/LT.png"));
+            images = ImageIO.read(new File("./images/x.png"));
+            imagec = ImageIO.read(new File("./images/settings.png"));
+            ImageIcon myImage = new ImageIcon(images);
+            set = new JButton(myImage);
+            myImage = new ImageIcon(imagec);
+            close = new JButton(myImage);
+         } 
+         catch (IOException exp) {
+            exp.printStackTrace();
+         }
+                        
+        // ImageIcon ii = new ImageIcon(this.getClass().getResource("./images/day.gif"));
+         //world.setIcon(ii);
+         //world.setBounds(320,320,268,250);
+        // add(world, 0);
+               
+               // this sets up the custom red x we put in the UI
+               //
+               //
+               //
+         set.setBounds(570,260,55,55);
+         set.setOpaque(false);
+         set.setContentAreaFilled(false);
+         set.setBorderPainted(false);
+         set.addActionListener(
+               new ActionListener(){
+                  @Override
+                           public void actionPerformed(ActionEvent e){
+                     System.exit(0);
+                  }
+               });
+               
+         add(set,0);
+               
+               
+               // this sets up the custom menu button
+               //
+               //
+         JPopupMenu popup = new JPopupMenu();
+         popup.add(new JMenuItem(
+               new AbstractAction("One Minute Timer") {
+                  public void actionPerformed(ActionEvent e) {
+                     JOptionPane.showMessageDialog(null, "Option 1 selected");
+                  }
+               }));
+         close.setBounds(570,600,55,55);
+         close.setOpaque(false);
+         close.setContentAreaFilled(false);
+         close.setBorderPainted(false);
+         close.addMouseListener(
+               new MouseAdapter(){
+                  @Override
+                           public void mousePressed(MouseEvent e){
+                     popup.show(e.getComponent(), e.getX(), e.getY());
+                  }
+               });
+               
+         add(close,4);
+         angle = value;
+         Timer timer = new Timer(60000, 
+               new ActionListener() {
+                  @Override
+                           public void actionPerformed(ActionEvent e) {
+                     angle += .25;
+                     if (angle > 360) {
+                        angle -= 360;
+                     }
+                     repaint();
+                  }
+               });
+         timer.setRepeats(true);
+         timer.setCoalesce(true);
+         timer.start();
+      }
+   
+   
+               
+   
+      @Override
+      public Dimension getPreferredSize() {
+         int width = circle.getHeight() + indicator.getWidth();
+         int height = circle.getHeight() + indicator.getWidth();
+      
+         return new Dimension(width * 2, height * 2);
+      }
+   
+      protected Point getCircleStart() {
+      
+         int radius = 150; // This is the height of the circle...
+      
+         int x = Math.round(getWidth() / 2);
+         int y = Math.round(getHeight() / 2);
+      
+         double rads = Math.toRadians(angle); // Make 0 point out to the right...
+         // If you add indicator.getWidth, you might be able to change the above...
+         int fullLength = Math.round((radius / 2)) ;
+      
+         // Calculate the outter point of the line
+         int xPosy = Math.round((float) (x + Math.cos(rads) * fullLength-10));
+         int yPosy = Math.round((float) (y - Math.sin(rads) * fullLength));
+      
+         return new Point(xPosy, yPosy);
+      
+      }
+   
+      @Override
+      protected void paintComponent(Graphics g) {
+      
+         super.paintComponent(g);
+      
+         Graphics2D g2d = (Graphics2D) g.create();
+      
+         int x = (getWidth() - circle.getWidth()) / 2;
+         int y = (getHeight() - circle.getHeight()) / 2;
+      
+         g2d.drawImage(circle, x, y, this);
+      
+         x = getWidth() / 2;
+         y = getHeight() / 2;
+      
+         Point p = getCircleStart();
+         g2d.setColor(Color.BLUE);
+         g2d.drawLine(x, y, p.x, p.y);
+      
+         AffineTransform at = AffineTransform.getTranslateInstance(p.x, p.y);
+         at.rotate(Math.toRadians(-angle));
+         g2d.setTransform(at);
+         g2d.drawImage(indicator, 50, -25, this);
+         
+      
+         g2d.dispose();
+      
+      }
+   }
+}
--- TimeClient.java
+++ TimeClient.java
@@ -11,37 +11,17 @@ public class TimeClient
      
    public static void main(String[] args) throws Exception {
       
-      Integer check = 1;
+      Integer check = 0;
       Scanner scanner = new Scanner(System.in);
-      String time = "";
+      String time = "129.6.15.30";
       String date = new String();
       Integer ms = new Integer(0);   
-      timeNode<String, String, Integer> bam = null;
-     
-   //driver
-      while(check != 3){
-         System.out.println("1. enter an NTP server.\n2. Print out the Time node. \n3. Exit.");
-         check = scanner.nextInt();
-         switch(check){
-            case 1:
-               System.out.println("Enter your server: ");
-               time = scanner.next();
-               bam = new timeNode<String, String, Integer>(time,date,ms);
-               System.out.println("here");
-               new input(bam);
-               break; //Here I want to break the while loop
-            case 2:
-               System.out.println("\nFull toString: \n" + bam.toString() 
-                  + "\nserver name tostring: " + bam.getInput() + "\nserver date tostring: " + bam.getDate() 
-                  + "\nTime in miliseconds of reciept name tostring: " + bam.getTime()+ "\n");
-               break;
-            case 3:
-               System.exit(0);  
-            default:
-               System.out.println("No such choice");
-         }
-      }
-         
+      timeNode<String, String, Integer> bam = new timeNode<String, String, Integer>(time,date,ms);
+      new input(bam);
+      check = (bam.getTime()/240000) +270;
+      System.out.println(check);
+      new ClockUI(check);
+        
       
    }
 }
@@ -57,26 +37,12 @@ class input
       
       try{
          NTPUDPClient timeClient = new NTPUDPClient();
-         System.out.println("sending input to InetAddress");
-         System.out.println(alan.getInput());
          InetAddress inetAddress = InetAddress.getByName(alan.getInput());
-         System.out.println("Sending InetAddress to getTime");
-         
-         System.out.println(inetAddress.toString());
-         
          TimeInfo timeInfo = timeClient.getTime(inetAddress);
-         System.out.println("retrieved the pack and then setting it to milliseconds");
          long returnTime = timeInfo.getMessage().getTransmitTimeStamp().getTime();
-         System.out.println("success!");
          Date time = new Date(returnTime);
-      
-         System.out.println("Time from " + alan.getInput() + ": " + time);
-        
-      //sets the date field in the time node
          String date = time.toString().substring(0, 10) + time.toString().substring(23, time.toString().length());
          alan.setDate(date);
-      
-      //sets the time field in milliseconds
          Integer setTime = (Integer.parseInt(time.toString().substring(11,13)) * 60 * 60 * 1000)
             + (Integer.parseInt(time.toString().substring(14,16)) * 60 * 1000)
             + (Integer.parseInt(time.toString().substring(17,19)) * 1000);
